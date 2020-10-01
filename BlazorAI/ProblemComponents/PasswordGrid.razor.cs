ebb255e406ca9c973dc5a6b17760e7e05c568dd1
lhalsey <halseylm@gmail.com>
2020-10-01 08:40:29 +0100

    Make password grid into separate component

---
+++ BlazorAI/ProblemComponents/PasswordGrid.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorAI.Client.ProblemComponents
+{
+    public partial class PasswordGrid
+    {
+        [Parameter]
+        public string Solution { get; set; }
+
+        [Parameter]
+        public int RowLength { get; set; }
+    }
+}
